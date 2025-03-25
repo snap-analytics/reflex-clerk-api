aed3e5a29d6ed08240ec81b0532cda73d2b35756
Tim Child <timjchild@gmail.com>
2025-03-25 16:06:10 -0700

    fix ci batch and version bumping

--- scripts/bump-version.py
+++ scripts/bump-version.py
@@ -34,20 +34,37 @@ def update_toml_project_version(file_path: Path, new_version: str) -> None:
         file.write(tomlkit.dumps(data))
 
 
-def main(part: LITERAL_PART, pyproject_path: Path) -> None:
+def update_readme_badge(readme_path: Path, new_version: str) -> None:
+    with readme_path.open("r") as file:
+        lines = file.readlines()
+
+    if lines:
+        # Update the first line with the new version
+        lines[0] = (
+            f"![Test Status](https://github.com/TimChild/reflex-clerk-api/actions/workflows/ci.yml/badge.svg?branch=v{new_version})\n"
+        )
+
+    with readme_path.open("w") as file:
+        file.writelines(lines)
+
+
+def main(part: LITERAL_PART, pyproject_path: Path, readme_path: Path) -> None:
     current_version = get_version_from_pyproject_toml(pyproject_path)
     new_version = bump_version(current_version, part)
     update_toml_project_version(pyproject_path, new_version)
+    update_readme_badge(readme_path, new_version)
     print(f"Successfully bumped from {current_version} to {new_version}")
 
 
 if __name__ == "__main__":
-    if len(sys.argv) != 3:
+    if len(sys.argv) != 4:
         raise ValueError(
-            "Usage: bump-version.py <major|minor|patch> <pyproject.toml-path>"
+            "Usage: bump-version.py <major|minor|patch> <pyproject.toml-path> <readme-path>"
         )
     part = sys.argv[1]
     assert part in ("major", "minor", "patch")
-    path = sys.argv[2]
-    assert Path(path).exists()
-    main(part, Path(path))
+    pyproject_path = Path(sys.argv[2])
+    assert pyproject_path.exists()
+    readme_path = Path(sys.argv[3])
+    assert readme_path.exists()
+    main(part, pyproject_path, readme_path)
